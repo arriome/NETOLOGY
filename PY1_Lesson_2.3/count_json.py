d91d4881f705d0b1b26f72ddd51aa62657e90bf9
Roman V <39599043+arriome@users.noreply.github.com>
2018-09-17 07:20:33 +0300

    Update count_json.py

--- PY1_Lesson_2.3/count_json.py
+++ PY1_Lesson_2.3/count_json.py
@@ -1,16 +1,19 @@
 import re
 import string
-import collections
-import sys
+#import sys
+import chardet  
 from collections import Counter
 def count(file):
-    frequency = {}
-    
+    #frequency = {}    
     #sys.setdefaultencoding('utf-8')
-    #with open(file, "r", encoding="utf-8") as f:
-    with open(file, "r") as f:
-        text_string = f.read().lower()
-        match_pattern = re.findall(r'\b[a-z]{6,}\b', text_string)
+    #with open(file, "r", encoding="utf-8") as f:  
+    with open(file, "rb") as f:        
+        text_string = f.read()
+        result = chardet.detect(text_string)        
+        text_enc = result['encoding']        
+    with open(file, "r", text_enc) as f2:     
+        text_string2 = f2.read().lower()
+        match_pattern = re.findall(r'\b[a-z]{6,}\b', text_string2)
     c = collections.Counter(match_pattern)
     print("File name:",file)
     print(c.most_common(10))
